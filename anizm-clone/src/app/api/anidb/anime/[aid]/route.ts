12ca93f6f1e68e73d2459e945d68ab9ec8fc14ab
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-10-29 13:11:31 +0300

    main

--- anizm-clone/src/app/api/anidb/anime/[aid]/route.ts
+++ anizm-clone/src/app/api/anidb/anime/[aid]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getAnimeById } from "../../lib/anidb";
+import { getAnimeById } from "../lib/anidb";
 
 export async function GET(_:Request, { params }:{params:{aid:string}}){
   try{
