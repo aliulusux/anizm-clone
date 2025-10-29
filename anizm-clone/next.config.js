fa28ff353d13837d29fc68d04b95dc24ecc312ae
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-10-29 12:41:49 +0300

    main

--- anizm-clone/next.config.js
+++ anizm-clone/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'cdn.anidb.net' }] }
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'cdn.anidb.net' }],
+  },
 };
 module.exports = nextConfig;
